e7028e1e92110535ba66d1bb0b40417b7f28adce
seodev <104405313+seodevv@users.noreply.github.com>
2023-05-23 21:17:01 +0900

    Add files via upload

---
+++ react/manual install/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+
+module.exports = {
+  name: "WordrRlay-setting",
+  mode: "development", // 실서비스: production
+  devtool: "eval", // hidden-source-map
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+
+  entry: {
+    app: ["./client"],
+  }, // 입력
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  browsers: ["> 5% in KR", "last 2 chrome versions"],
+                },
+              },
+            ],
+            "@babel/preset-react",
+          ],
+        },
+      },
+    ],
+  },
+
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "app.js",
+  }, // 출력
+};
