42a63b2301e8eb24056de982712b28720a7b95aa
seodev <104405313+seodevv@users.noreply.github.com>
2023-05-23 21:16:29 +0900

    Update command.sh

--- react/manual install/command.sh
+++ react/manual install/command.sh
@@ -31,4 +31,45 @@ npm i -D webpack webpack-cli
   }
 }
 
+(webpack.config.js)
+const path = require("path");
 
+module.exports = {
+  name: "WordrRlay-setting",
+  mode: "development", // 실서비스: production
+  devtool: "eval", // hidden-source-map
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+
+  entry: {
+    app: ["./client"],
+  }, // 입력
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  browsers: ["> 5% in KR", "last 2 chrome versions"],
+                },
+              },
+            ],
+            "@babel/preset-react",
+          ],
+        },
+      },
+    ],
+  },
+
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "app.js",
+  }, // 출력
+};
