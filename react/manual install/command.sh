eb17f61050ed6c9411efc92ae9d6d12a1c06d22d
seodev <104405313+seodevv@users.noreply.github.com>
2023-05-23 21:19:40 +0900

    Update command.sh

--- react/manual install/command.sh
+++ react/manual install/command.sh
@@ -1,75 +1,17 @@
+# 신규 react 프로젝트 생성
 mkdir manual-react
 cd manual-react
 
+# package.json 파일 생성
 npm init
+
+# react 모듈 설치
 npm i react react-dom
+
+# babel 모듈 및 로더 설치
+# babel 은 브라우저 간의 호환성을 해결해주는 패키지로 webpack 과 같이 쓰인다.
 npm i -D @babel/core @babel/preset-env @babel/preset-react babel-loader
+
+# webpack 모듈 설치
+# webpack 은 여러 js, jsx 파일 등을 하나의 js 파일로 합쳐주는 패키지이다.
 npm i -D webpack webpack-cli
-
-(package.json)
-{
-  "name": "2.wordrelay",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "dev": "webpack"
-  },
-  "author": "seodevv",
-  "license": "ISC",
-  "dependencies": {
-    "react": "^18.2.0",
-    "react-dom": "^18.2.0"
-  },
-  "devDependencies": {
-    "@babel/core": "^7.21.8",
-    "@babel/preset-env": "^7.21.5",
-    "@babel/preset-react": "^7.18.6",
-    "babel-loader": "^9.1.2",
-    "webpack": "^5.83.1",
-    "webpack-cli": "^5.1.1"
-  }
-}
-
-(webpack.config.js)
-const path = require("path");
-
-module.exports = {
-  name: "WordrRlay-setting",
-  mode: "development", // 실서비스: production
-  devtool: "eval", // hidden-source-map
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-
-  entry: {
-    app: ["./client"],
-  }, // 입력
-
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: "babel-loader",
-        options: {
-          presets: [
-            [
-              "@babel/preset-env",
-              {
-                targets: {
-                  browsers: ["> 5% in KR", "last 2 chrome versions"],
-                },
-              },
-            ],
-            "@babel/preset-react",
-          ],
-        },
-      },
-    ],
-  },
-
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: "app.js",
-  }, // 출력
-};
